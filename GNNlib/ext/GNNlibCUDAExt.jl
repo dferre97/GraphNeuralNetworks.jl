0221593db26f3e6c3b7967005afb87086e0e256a
dferre97 <92430019+dferre97@users.noreply.github.com>
2025-07-06 19:01:50 +0200

    CUDA copy_xj propagate sparse support and benchmarks (#605)
    * Added propagate copy_xj CUDA sparse support using matrix mul
    
    * Add benchmarks for CUDA sparse propagate copy_xj
    
    * Keep old gather/scatter implementation for COO_T

--- GNNlib/ext/GNNlibCUDAExt.jl
+++ GNNlib/ext/GNNlibCUDAExt.jl
@@ -10,7 +10,7 @@ using GNNGraphs: GNNGraph, COO_T, SPARSE_T
 ## COPY_XJ 
 
 ## avoid the fast path on gpu until we have better cuda support
-function GNNlib.propagate(::typeof(copy_xj), g::GNNGraph{<:Union{COO_T, SPARSE_T}}, ::typeof(+),
+function GNNlib.propagate(::typeof(copy_xj), g::GNNGraph{COO_T}, ::typeof(+),
         xi, xj::AnyCuMatrix, e)
     propagate((xi, xj, e) -> copy_xj(xi, xj, e), g, +, xi, xj, e)
 end
