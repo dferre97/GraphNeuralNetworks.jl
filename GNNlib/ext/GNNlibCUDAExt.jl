7274e048b7c5e4e158df979452a10a8e708562c6
dferre97 <davide.ferre1997@gmail.com>
2025-08-31 10:31:34 +0200

    Add @non_differentiable annotation to _adjacency_matrix function

--- GNNlib/ext/GNNlibCUDAExt.jl
+++ GNNlib/ext/GNNlibCUDAExt.jl
@@ -4,6 +4,7 @@ using CUDA
 using Random, Statistics, LinearAlgebra
 using GNNlib: GNNlib, propagate, copy_xj, e_mul_xj, w_mul_xj
 using GNNGraphs: GNNGraph, COO_T, SPARSE_T, to_dense, to_sparse
+using ChainRulesCore: @non_differentiable
 
 const CUDA_COO_T = Tuple{T, T, V} where {T <: AnyCuArray{<:Integer}, V <: Union{Nothing, AnyCuArray}}
 
@@ -62,5 +63,7 @@ function _adjacency_matrix(g::GNNGraph{<:CUDA_COO_T}, T::DataType = eltype(g); d
     @assert size(A) == (n, n)
     return dir == :out ? A : A'
 end
+
+@non_differentiable _adjacency_matrix(x...)
 
 end #module
